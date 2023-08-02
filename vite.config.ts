649e3bd03e2ab725e35f7e0beec10485bbc66ab0
Maurício Bruno <mb.bruno.s@gmail.com>
2023-08-02 15:22:33 -0300

    feat: configure project

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import react from '@vitejs/plugin-react'
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import EsLint from 'vite-plugin-linter'
+import * as packageJson from './package.json'
+
+const { EsLinter, linterPlugin } = EsLint
+
+// https://vitejs.dev/config/
+export default defineConfig((configEnv) => ({
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    linterPlugin({
+      include: ['./src}/**/*.{ts,tsx}'],
+      linters: [new EsLinter({ configEnv })],
+    }),
+    dts({
+      include: ['src/'],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve('src', 'index.ts'),
+      name: 'arc-carousel',
+      formats: ['es', 'umd'],
+      fileName: (format) => `arc-carousel.${format}.js`,
+    },
+    rollupOptions: {
+      external: [...Object.keys(packageJson.peerDependencies)],
+    },
+  },
+}))
