ce3acb50dd3182e71228851b654f3d584595af51
Maurício Bruno <mb.bruno.s@gmail.com>
2023-08-04 18:02:56 -0300

    feat: enhance base component structure

--- src/components/CarouselRoot.tsx
+++ src/components/CarouselRoot.tsx
@@ -1,20 +1,51 @@
-import React, { HTMLAttributes, useState } from 'react'
-import { twMerge } from 'tailwind-merge'
+import React, { HTMLAttributes, forwardRef, useState } from 'react'
+import { cva, type VariantProps } from 'class-variance-authority'
 import { CarouselContext } from 'src/context/CarouselContext'
+import { Slot } from '@radix-ui/react-slot'
+import { cn } from '@/lib/utils'
 
-export type CarouselRootProps = HTMLAttributes<HTMLDivElement> & {
-  variant?: 'default' | 'full'
+export const carouselRootVariants = cva('flex overflow-x-auto', {
+  variants: {
+    variant: {
+      default: 'flex w-full gap-2 overflow-x-auto',
+      vertical: 'flex flex-col overflow-y-auto',
+      full: 'flex flex-col overflow-y-auto',
+    },
+  },
+  defaultVariants: {
+    variant: 'default',
+  },
+})
+
+export interface CarouselRootProps
+  extends HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof carouselRootVariants> {
+  asChild?: boolean
 }
 
-export const CarouselRoot = ({ variant, children, className, ...props }: CarouselRootProps) => {
-  const [activeIndex, setActiveIndex] = useState(0)
-  const slideCount = React.Children.count(children)
+export const CarouselRoot = forwardRef<HTMLDivElement, CarouselRootProps>(
+  ({ variant, className, children, asChild = false, ...props }, ref) => {
+    const [currentSlide, setCurrentSlide] = useState(0)
+    const slideCount = React.Children.count(children)
 
-  return (
-    <CarouselContext.Provider value={{ variant, activeIndex, setActiveIndex, slideCount }}>
-      <div className={twMerge('flex overflow-x-auto', className)} {...props}>
-        {children}
-      </div>
-    </CarouselContext.Provider>
-  )
-}
+    const nextSlide = () => {
+      setCurrentSlide((prev) => (prev + 1) % slideCount)
+    }
+
+    const prevSlide = () => {
+      setCurrentSlide((prev) => (prev - 1 + slideCount) % slideCount)
+    }
+
+    const Comp = asChild ? Slot : 'div'
+
+    return (
+      <CarouselContext.Provider value={{ variant, currentSlide, prevSlide, nextSlide, slideCount }}>
+        <Comp className={cn(carouselRootVariants({ variant, className }))} ref={ref} {...props}>
+          {children}
+        </Comp>
+      </CarouselContext.Provider>
+    )
+  },
+)
+
+CarouselRoot.displayName = 'CarouselRoot'
