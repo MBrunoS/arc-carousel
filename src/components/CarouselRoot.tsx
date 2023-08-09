dfc9c35ab354c1548b96c4108ed85f1854f8b7bc
Maurício Bruno <mb.bruno.s@gmail.com>
2023-08-08 21:08:17 -0300

    refactor: move common functions to context

--- src/components/CarouselRoot.tsx
+++ src/components/CarouselRoot.tsx
@@ -1,6 +1,6 @@
-import React, { HTMLAttributes, forwardRef, useState } from 'react'
+import React, { HTMLAttributes, forwardRef } from 'react'
 import { cva, type VariantProps } from 'class-variance-authority'
-import { CarouselContext } from 'src/context/CarouselContext'
+import { CarouselProvider } from 'src/context/CarouselContext'
 import { Slot } from '@radix-ui/react-slot'
 import { cn } from '@/lib/utils'
 
@@ -25,25 +25,15 @@ export interface CarouselRootProps
 
 export const CarouselRoot = forwardRef<HTMLDivElement, CarouselRootProps>(
   ({ variant, className, children, asChild = false, ...props }, ref) => {
-    const [currentSlide, setCurrentSlide] = useState(0)
+    const Comp = asChild ? Slot : 'div'
     const slideCount = React.Children.count(children)
 
-    const nextSlide = () => {
-      setCurrentSlide((prev) => (prev + 1) % slideCount)
-    }
-
-    const prevSlide = () => {
-      setCurrentSlide((prev) => (prev - 1 + slideCount) % slideCount)
-    }
-
-    const Comp = asChild ? Slot : 'div'
-
     return (
-      <CarouselContext.Provider value={{ variant, currentSlide, prevSlide, nextSlide, slideCount }}>
+      <CarouselProvider slideCount={slideCount}>
         <Comp className={cn(carouselRootVariants({ variant, className }))} ref={ref} {...props}>
           {children}
         </Comp>
-      </CarouselContext.Provider>
+      </CarouselProvider>
     )
   },
 )
--- src/context/CarouselContext.tsx
+++ src/context/CarouselContext.tsx
@@ -1,10 +1,43 @@
-import { createContext } from 'react'
+import React, { createContext, useCallback, useState } from 'react'
 
-type CarouselContextType = {
-  variant?: 'default' | 'full'
-  activeIndex: number
-  setActiveIndex: (index: number) => void
+interface CarouselContextType {
+  variant?: 'default' | 'full' | 'vertical' | null
+  currentSlide: number
+  nextSlide: () => void
+  prevSlide: () => void
   slideCount: number
 }
 
 export const CarouselContext = createContext<CarouselContextType>({} as CarouselContextType)
+
+export interface CarouselProviderProps {
+  children: React.ReactNode
+  slideCount: number
+}
+
+export const CarouselProvider = ({ children, slideCount }: CarouselProviderProps) => {
+  const [currentSlide, setCurrentSlide] = useState(0)
+
+  const nextSlide = useCallback(() => {
+    setCurrentSlide((prev) => (prev + 1) % slideCount)
+  }, [])
+
+  const prevSlide = useCallback(() => {
+    setCurrentSlide((prev) => (prev - 1 + slideCount) % slideCount)
+  }, [])
+
+  return (
+    <CarouselContext.Provider
+      value={{
+        currentSlide,
+        nextSlide,
+        prevSlide,
+        slideCount,
+      }}
+    >
+      {children}
+    </CarouselContext.Provider>
+  )
+}
+
+CarouselProvider.displayName = 'CarouselProvider'
