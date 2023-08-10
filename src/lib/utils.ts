2b9cab86dacf3174b4c9ad5e94ffc9fd406091f8
Maurício Bruno <mb.bruno.s@gmail.com>
2023-08-09 21:25:09 -0300

    feat: add children filter helper function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
 import { clsx, type ClassValue } from 'clsx'
+import React from 'react'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+// Filter children by component type
+export function filterChildren<T>(children: React.ReactNode, validationType: T) {
+  return React.Children.toArray(children).filter((child) => {
+    return React.isValidElement(child) && (child as React.ReactElement).type === validationType
+  })
 }
