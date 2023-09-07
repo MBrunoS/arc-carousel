f1cbc5ded24cfa0087329ba3d9e0f7781a655a13
Maurício Bruno <mb.bruno.s@gmail.com>
2023-09-07 17:00:48 -0300

    build(web): add deps and tailwind

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Carousel } from '@mbrunos/arc-carousel'
+import '@mbrunos/arc-carousel/styles.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <section className="h-screen p-4 lg:px-20 lg:py-10">
+      <Carousel.Root>
+        <Carousel.Wrapper>
+          <Carousel.Slide className="flex items-center justify-center bg-indigo-300">
+            SLIDE 1
+          </Carousel.Slide>
+          <Carousel.Slide className="flex items-center justify-center bg-indigo-400">
+            SLIDE 2
+          </Carousel.Slide>
+          <Carousel.Slide className="flex items-center justify-center bg-indigo-300">
+            SLIDE 3
+          </Carousel.Slide>
+          <Carousel.Slide className="flex items-center justify-center bg-indigo-400">
+            SLIDE 4
+          </Carousel.Slide>
+          <Carousel.Slide className="flex items-center justify-center bg-indigo-300">
+            SLIDE 5
+          </Carousel.Slide>
+        </Carousel.Wrapper>
+      </Carousel.Root>
+    </section>
   )
 }
 
