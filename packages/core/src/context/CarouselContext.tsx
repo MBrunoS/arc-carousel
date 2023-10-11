9a756298a66d878d8ad7c411316880f784f774d0
Maurício Bruno <mb.bruno.s@gmail.com>
2023-10-11 18:04:31 -0300

    fix(core): solve autoplay moving to old index

--- packages/core/src/context/CarouselContext.tsx
+++ packages/core/src/context/CarouselContext.tsx
@@ -105,7 +105,7 @@ export const CarouselProvider = ({
     return () => {
       clearInterval(autoplayTimerRef.current!)
     }
-  }, [isAutoplaying])
+  }, [isAutoplaying, next])
 
   return (
     <CarouselContext.Provider
